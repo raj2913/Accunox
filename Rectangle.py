6b1248fbb873e9a8995f868c46c3db6a343ffa25
raj2913 <142222769+raj2913@users.noreply.github.com>
2024-09-22 21:42:59 +0530

    Add files via upload

---
+++ Rectangle.py
@@ -0,0 +1,15 @@
+class Rectangle:
+    def __init__(self, length: int, width: int):
+        self.length = length
+        self.width = width
+
+    def __iter__(self):
+        yield {'length': self.length}
+        yield {'width': self.width}
+
+# Example usage:
+rectangle = Rectangle(10, 5)
+
+# Iterating over the instance
+for dimension in rectangle:
+    print(dimension)
